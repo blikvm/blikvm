91409dc7d426f738e526937d4ee7c330a57ec8a0
ThomasVon2021 <1767893964@qq.com>
2025-07-30 15:22:50 +0100

    1. update web picture; 2. update package dependence: libxkbcommon0; 3. change app.json path to relative;

--- src/demo.c
+++ src/demo.c
@@ -9,7 +9,7 @@
 
 
 // static char* config_json_path= (char*)"/mnt/blikvm/web_src/web_server/config/app.json";
-static char* config_json_path= (char*)"/mnt/exec/release/config/app.json";
+static char* config_json_path= (char*)"./config/app.json";
 static blikvm_log_t g_log_config={0};
 static blikvm_config_t* g_config = NULL;
 
