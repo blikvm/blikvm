70650cbf3227886b7b534bb4a88c484568387073
blikvm <1767893964@qq.com>
2025-11-05 07:04:57 +0000

    chore: update make_image_prework.sh

--- script/make_image_prework.sh
+++ script/make_image_prework.sh
@@ -5,6 +5,10 @@ set -x
 # touch firstboot file
 touch /mnt/tmp/firstboot
 
+# make sure i2c-8 is enabled in config.txt for v5 hardware
+#CFG="/boot/firmware/config.txt"
+#dtoverlay=i2c-gpio,bus=8,i2c_gpio_sda=22,i2c_gpio_scl=23
+
 # clean history command
 history -c && history -w
 
